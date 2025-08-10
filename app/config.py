f46413a2c4f51f3571ea4301068a1098347e315c
Aman Jha <amanj@pronnel.com>
2025-08-11 01:03:13 +0530

    taking api from env file

--- app/config.py
+++ app/config.py
@@ -1,2 +1,4 @@
+import os
+
 class Config:
-    API_KEY = "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"  # Default API key (override with env vars)
+    API_KEY = os.getenv("API_KEY")  # Default API key (override with env vars)

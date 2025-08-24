136046899835ad39198b13ea72ccdffb046047f9
Aman Jha <amanj@pronnel.com>
2025-08-24 22:33:11 +0530

    and public id in g

--- app/middleware/__init__.py
+++ app/middleware/__init__.py
@@ -2,6 +2,8 @@ import os
 import jwt
 from flask import request, current_app
 from functools import wraps
+from loguru import logger
+from flask import g
 
 # Function to validate API Key
 def validate_api_key(request):
@@ -11,6 +13,8 @@ def validate_api_key(request):
     secret_key = current_app.config.get("SECRET_KEY", "default_secret_key")
     try:
         decoded = jwt.decode(api_key, secret_key, algorithms=["HS256"])
+        g.public_id = decoded.get("public_id")
+        logger.debug(f"Decoded JWT: {decoded}")
     except jwt.ExpiredSignatureError:
         return False
     except jwt.InvalidTokenError:

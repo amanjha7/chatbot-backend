110da3a71b19582a4671e4f3c7976627f33863b6
Aman Jha <amanj@pronnel.com>
2025-08-24 21:48:19 +0530

    added my secret key in config

--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@ import os
 
 class Config:
     API_KEY = os.getenv("API_KEY")  # Default API key (override with env vars)
+    SECRET_KEY = os.getenv("SECRET_KEY")  # Secret key for session management
